1d3f136e07977459991e8cf92ecc1be09a7664b7
Nyall Dawson <nyall.dawson@gmail.com>
2026-08-11 12:58:34 +1000

    Expand test coverage

--- topographic_mapping/test/test_project_controller.py
+++ topographic_mapping/test/test_project_controller.py
@@ -5,6 +5,9 @@ import unittest
 from qgis.PyQt.QtCore import QVariant
 from qgis.core import (
     Qgis,
+    QgsFeature,
+    QgsGeometry,
+    QgsPointXY,
     QgsProject,
     QgsValueMapFieldFormatter,
     QgsField,
@@ -15,7 +18,7 @@ from qgis.core import (
     QgsVectorLayer,
 )
 
-from topographic_mapping.core import ProjectController
+from topographic_mapping.core import ProjectController, EditMode
 from .test_base import TopographicTestBase
 from .utilities import get_qgis_app
 
@@ -61,6 +64,16 @@ class TestProjectController(TopographicTestBase):
         )
 
         return layer
+
+    def test_clean_layer_name(self):
+        """Tests cleaning product view layer name suffixes."""
+        self.assertEqual(
+            ProjectController.clean_layer_name("water_point_product_view"),
+            "water_point",
+        )
+        self.assertEqual(
+            ProjectController.clean_layer_name("water_point"), "water_point"
+        )
 
     def test_layer_schema_configuration(self):
         """Tests that schemas are loaded from disk and widget setups are applied to fields."""
@@ -151,6 +164,148 @@ class TestProjectController(TopographicTestBase):
         self.assertEqual(
             controller.layer_for_feature_type("water_point").name(), "water point"
         )
+
+    def test_map_sheet_layer_and_signals(self):
+        """
+        Tests map sheet layer retrieval and signal emissions on add/remove.
+        """
+        project = QgsProject()
+        controller = ProjectController(project, None)
+
+        loaded_signal_emitted = False
+        unloaded_signal_emitted = False
+
+        def on_loaded():
+            nonlocal loaded_signal_emitted
+            loaded_signal_emitted = True
+
+        def on_unloaded():
+            nonlocal unloaded_signal_emitted
+            unloaded_signal_emitted = True
+
+        controller.map_sheet_layer_loaded.connect(on_loaded)
+        controller.map_sheet_layer_unloaded.connect(on_unloaded)
+
+        self.assertIsNone(controller.map_sheet_layer())
+
+        fields = QgsFields()
+        fields.append(QgsField("type", QVariant.String))
+        map_sheet_layer = self.create_dummy_layer(
+            ProjectController.MAP_SHEET_LAYER_NAME, fields
+        )
+        project.addMapLayer(map_sheet_layer)
+
+        self.assertTrue(loaded_signal_emitted)
+        self.assertEqual(controller.map_sheet_layer(), map_sheet_layer)
+
+        project.removeMapLayer(map_sheet_layer.id())
+        self.assertTrue(unloaded_signal_emitted)
+        self.assertIsNone(controller.map_sheet_layer())
+
+    def test_working_geopackage_path_and_editable_layers(self):
+        """
+        Tests determining working GeoPackage path and fetching editable layers in GPKG.
+        """
+        project = QgsProject()
+        controller = ProjectController(project, None)
+
+        gpkg_path = controller.working_geopackage_path()
+        self.assertIsNone(gpkg_path)
+
+        fields = QgsFields()
+        fields.append(QgsField("id", QVariant.Int))
+        fields.append(QgsField("type", QVariant.String))
+
+        layer1 = self.create_dummy_layer("NOT_A_FEATURE_TYPE", fields)
+        project.addMapLayer(layer1)
+
+        gpkg_path = controller.working_geopackage_path()
+        self.assertIsNone(gpkg_path)
+        project.removeMapLayer(layer1.id())
+
+        layer2 = self.create_dummy_layer("water_point", fields)
+        project.addMapLayer(layer2)
+
+        gpkg_path = controller.working_geopackage_path()
+        self.assertIsNotNone(gpkg_path)
+        self.assertTrue(gpkg_path.endswith(".gpkg"))
+
+        layers_in_gpkg = list(controller.editable_vector_layers_in_gpkg(gpkg_path))
+        self.assertEqual(len(layers_in_gpkg), 1)
+        self.assertEqual(layers_in_gpkg[0], layer2)
+
+    def test_geometry_and_attribute_changed_slots(self):
+        """
+        Tests automatic tracking of version and change_type on feature edits.
+        """
+        fields = QgsFields()
+        fields.append(QgsField("id", QVariant.Int))
+        fields.append(QgsField("name", QVariant.String))
+        fields.append(QgsField("change_type", QVariant.String))
+        fields.append(QgsField("version", QVariant.Int))
+
+        layer = self.create_dummy_layer("water_point", fields)
+        project = QgsProject()
+        project.addMapLayer(layer)
+        _ = ProjectController(project, None)
+
+        layer.startEditing()
+        feat = QgsFeature(layer.fields())
+        feat["id"] = 1
+        feat["name"] = "test"
+        feat["change_type"] = "new"
+        feat["version"] = 1
+        feat.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(0, 0)))
+        layer.addFeature(feat)
+        layer.commitChanges()
+
+        fid = next(layer.getFeatures()).id()
+
+        # trigger attribute change on edited feature
+        layer.startEditing()
+        name_idx = layer.fields().lookupField("name")
+        layer.changeAttributeValue(fid, name_idx, "updated_name")
+
+        updated_feat = layer.getFeature(fid)
+        self.assertTrue(updated_feat.isValid())
+        self.assertEqual(updated_feat["change_type"], "modified attributes")
+        self.assertEqual(updated_feat["version"], 2)
+
+        # trigger geometry change on edited feature
+        layer.changeGeometry(fid, QgsGeometry.fromPointXY(QgsPointXY(1, 1)))
+
+        updated_feat = layer.getFeature(fid)
+        self.assertEqual(updated_feat["change_type"], "modified geometry and att")
+        self.assertEqual(updated_feat["version"], 2)
+        layer.commitChanges()
+
+        layer.startEditing()
+        layer.changeGeometry(fid, QgsGeometry.fromPointXY(QgsPointXY(1, 3)))
+
+        updated_feat = layer.getFeature(fid)
+        self.assertEqual(updated_feat["change_type"], "modified geometry and att")
+        self.assertEqual(updated_feat["version"], 3)
+        layer.commitChanges()
+
+    def test_set_edit_mode(self):
+        """
+        Tests switching layer edit modes between RealWorld and ProductData.
+        """
+        project = QgsProject()
+        fields = QgsFields()
+        fields.append(QgsField("type", QVariant.String))
+
+        layer = self.create_dummy_layer("water_point", fields)
+        project.addMapLayer(layer)
+
+        controller = ProjectController(project, None)
+        gpkg_path = controller.working_geopackage_path()
+
+        controller.set_edit_mode(gpkg_path, EditMode.ProductData)
+        self.assertIn("water_point_product_view", layer.source())
+
+        controller.set_edit_mode(gpkg_path, EditMode.RealWorld)
+        self.assertNotIn("water_point_product_view", layer.source())
 
 
 if __name__ == "__main__":
