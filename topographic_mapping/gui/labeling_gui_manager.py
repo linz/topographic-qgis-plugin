1cee4b5bb0dd8a3787c1d73eb1bd168ef8d8a7d7
Nyall Dawson <nyall.dawson@gmail.com>
2026-09-09 11:41:13 +1000

    Copy private qgis map tool code for pre-highlighting labels

--- topographic_mapping/gui/labeling_gui_manager.py
+++ topographic_mapping/gui/labeling_gui_manager.py
@@ -80,7 +80,7 @@ class LabelingGuiManager(QObject):
         self._reset_label_action.triggered.connect(self._reset_labels)
 
         self._rewrap_label_tool = RewrapLabelTool(
-            self._canvas, self._label_manager, self._project_controller
+            self._canvas, self._cad_dock, self._label_manager, self._project_controller
         )
 
         self._rewrap_label_action = tool_registry.custom_action(REWRAP_LABEL_ACTION)
